0228544a16289d75e07e82a97a3d6ce6781b171a
Vaud Keith <vaudkeith@gmail.com>
2022-09-20 20:47:53 +0000

    Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) in range(97, 123):
+            upper_value = ord(str[i]) - 32
+            if i + 1 < len(str):
+                print("{}".format(chr(upper_value)), end='')
+            else:
+                print(chr(upper_value))
+        else:
+            print(str[i], end='')
