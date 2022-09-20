53f0606477502834589364834b8561fb30167b7e
Vaud Keith <vaudkeith@gmail.com>
2022-09-20 21:11:01 +0000

    Task 8 print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,9 +5,7 @@ def uppercase(str):
     for i in range(len(str)):
         if ord(str[i]) in range(97, 123):
             upper_value = ord(str[i]) - 32
-            if i + 1 < len(str):
-                print("{}".format(chr(upper_value)), end='')
-            else:
-                print(chr(upper_value))
+            print("{}".format(chr(upper_value)), end = '')
         else:
-            print(str[i], end='')
+            print("{}".format(str[i]), end='');
+    print("") 
