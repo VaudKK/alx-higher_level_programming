b2d768ba90b67bc9b9a24215499f3846dc0a8b33
Vaud Keith <vaudkeith@gmail.com>
2022-09-21 09:04:04 +0000

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-for i in (0, 10):
-    for j in (1, 10):
-        if i != j:
-            print(str(i) + str(j), end=" ")
+for i in range(0, 10):
+    for y in range(0, 10):
+        if i < y and i != y:
+            if i != 8:
+                print("{:0<2s},".format(str(i) + str(y)), end=' ')
+            else:
+                print("{:0<2s}".format(str(i) + str(y)))
