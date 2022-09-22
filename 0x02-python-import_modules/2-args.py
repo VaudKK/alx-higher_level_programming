0b1b1307d30dbef0ffed508e82399514c4a7b8c0
Vaud Keith <vaudkeith@gmail.com>
2022-09-22 20:01:23 +0000

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
+    else:
+        print("0 arguments.")
