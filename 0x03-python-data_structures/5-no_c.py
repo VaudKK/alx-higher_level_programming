6dc5136ef031e6eac0bee59b3b4b61292bd198f6
Vaud Keith <vaudkeith@gmail.com>
2022-09-26 08:14:24 +0000

    Task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def no_c(my_string):
+    new_string = ""
+    for c in my_string:
+        if c == 'c' or c == 'C':
+            new_string += ' '
+        else:
+            new_string += c
+    return new_string
