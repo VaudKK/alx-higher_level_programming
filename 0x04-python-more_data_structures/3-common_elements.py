5334b442e8064836eba1f101cb7bcd3d33749abe
Vaud Keith <vaudkeith@gmail.com>
2022-09-28 20:33:19 +0000

    Task 3 fix

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,4 +2,4 @@
 
 
 def common_elements(set_1, set_2):
-    return set(x for x,y in zip(set_1, set_2) if x in set_1 and x in set_2)
+    return set(x for x, y in zip(set_1, set_2) if x in set_1 and x in set_2)
