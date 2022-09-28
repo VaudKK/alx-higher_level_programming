de2af42a4145e07ab84ca44267aea519fd920331
Vaud Keith <vaudkeith@gmail.com>
2022-09-28 20:48:14 +0000

    Task 4 fix

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,4 +2,6 @@
 
 
 def only_diff_elements(set_1, set_2):
-    return set([x for x in set_1 if x not in set_2] + [y for y in set_2 if y not in set_1])
+    list_1 = [x for x in set_1 if x not in set_2]
+    list_2 = [y for y in set_2 if y not in set_1]
+    return set(list_1 + list_2)
