15ef4f8d35b0309afcdefc977d7b93b8a1cc46da
Vaud Keith <vaudkeith@gmail.com>
2022-10-12 23:43:14 +0000

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""The square module"""
+
+
+class Square:
+    """Represents square"""
+
+    def __init__(self, size=0):
+        """Initialization of data"""
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        try:
+            int(size)
+        except TypeError:
+            raise TypeError('size must be an integer')
+
+        self.__size = size
+
+    def area(self):
+        """Get square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter"""
+        self.__size = value
+
+    def my_print(self):
+        """Print"""
+        if self.__size > 0:
+            print("#" * self.__size)
+
+        print()
