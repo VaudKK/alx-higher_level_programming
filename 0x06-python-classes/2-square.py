d762126355a2a23a282290e3f676ac5b79b7b6b9
Vaud Keith <vaudkeith@gmail.com>
2022-10-12 23:20:43 +0000

    Task 2 fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,12 +5,12 @@
 class Square:
     """Represents square"""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialization of data"""
         if size < 0:
             raise ValueError('size must be >= 0')
 
-        if type(size) != int:
+        if size is not int:
             raise TypeError('size must be an integer')
 
         self.__size = size
