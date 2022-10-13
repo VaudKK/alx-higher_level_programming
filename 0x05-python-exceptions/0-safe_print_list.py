dd7ade6c282a857c6d73a937e593d01b83f33189
Vaud Keith <Vaudkeith@gmail.com>
2022-10-13 13:57:49 -0700

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        printed = 0
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
+            printed += 1
+    except IndexError:
+        pass
+    finally:
+        print("")
+
+    return printed
