32bebc2c8b4b4ebbf3cfabb2d017e01e9c9c3dea
Vaud Keith <Vaudkeith@gmail.com>
2022-10-17 12:27:14 -0700

    Task 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+
+
+class Rectangle():
+    """The Rectangle Class"""
+
+    def __init__(self, width=0, height=0):
+        """initialize"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """width property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """height property"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """calculate area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """calculate perimeter"""
+        return 2 * (self.__height + self.__width)
