ce9b32e481743e9abc91cce9b96071681477d470
Vaud Keith <Vaudkeith@gmail.com>
2022-10-24 14:14:31 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" base geometry """
+
+
+class BaseGeometry():
+    """ base geo class"""
+
+    def area(self):
+        """ get area """
+        raise Exception("area() is not implemented")
