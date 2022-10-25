29ad56cdd2ba22d5e6128683dbd7f6b853887f90
Vaud Keith <Vaudkeith@gmail.com>
2022-10-25 06:04:31 -0700

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" base geometry """
+
+
+class BaseGeometry():
+    """ base geo class"""
+
+    def area(self):
+        """ get area """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validate integer """
+        if type(value) is not  int:
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
