8ee2f906f6e14704f0ecd1383b213b63d745d705
Vaud Keith <Vaudkeith@gmail.com>
2022-10-31 12:56:20 -0700

    Task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" rectangle module """
+from models.base import Base
+
+class Rectangle(Base):
+    """ Rectangle class """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initialize """
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """ getter width """
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """ setter width """
+        self.__width = width
+
+    @property
+    def height(self):
+        """ get height """
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """ set height """
+        self.__height = height
+
+    @property
+    def x(self):
+        """ get x """
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """ set x """
+        self.__x = x
+
+    @property
+    def y(self):
+        """ get y"""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """ set y """
+        self.__y = y
