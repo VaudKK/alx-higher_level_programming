657ff0c098651d9b0cd47064d5caae8b7520e7b0
Vaud Keith <Vaudkeith@gmail.com>
2022-11-01 07:12:41 -0700

    Task 13

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -133,3 +133,8 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 if key in my_attr:
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ to dictionary """
+        return {'id': self.id, 'width': self.__width, 'height': self.__height,
+                'x': self.__x, 'y': self.__y}
