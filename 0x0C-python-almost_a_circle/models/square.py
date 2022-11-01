2b2d880c37c4f8f88d64a258294257c1799f8c46
Vaud Keith <Vaudkeith@gmail.com>
2022-11-01 07:18:41 -0700

    Task 14 pep8

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -46,4 +46,4 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """ to dictionary """
-        return {'id': self.id, 'size': self.size,'x': self.x, 'y': self.y}
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
