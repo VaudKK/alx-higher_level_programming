28fb5c46ae73287b2054dde56d2afbba8bc79d77
Vaud Keith <Vaudkeith@gmail.com>
2022-11-30 13:45:51 -0800

    Task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- User 2 and db;
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON user_0d_2_pwd.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
