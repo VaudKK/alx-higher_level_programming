b47795fe8ba87dbf06e9c408c6e7fb786b44b337
Vaud Keith <Vaudkeith@gmail.com>
2022-11-30 14:09:54 -0800

    Task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- Db and states
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities(
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	state_id INT NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES states(id)
+	);
