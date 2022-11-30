801d3e4e4a087f53e8bd696e73abcb44969d3eb6
Vaud Keith <Vaudkeith@gmail.com>
2022-11-30 14:01:25 -0800

    Task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- Db and states
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE htbn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states(
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256) NOT NULL
+	);
