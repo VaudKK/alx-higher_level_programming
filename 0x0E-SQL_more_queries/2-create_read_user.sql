d12539eba1d3a87666cd347e5c1bdc5d1c11ba6c
Vaud Keith <Vaudkeith@gmail.com>
2022-11-30 13:49:04 -0800

    Task 2 adding usage grant

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,5 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
 GRANT SELECT ON user_0d_2_pwd.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
