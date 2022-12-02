1d953790d9646cd845fdfacdceb91a8838ea3aff
Vaud Keith <Vaudkeith@gmail.com>
2022-12-02 02:53:25 -0800

    Task 2 fix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
-GRANT SELECT ON user_0d_2_pwd.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
