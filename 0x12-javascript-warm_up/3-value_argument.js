b8c7e82c25d26891aecce153ca1d86d29b568ebc
VaudKK <vaudkeith@gmail.com>
2022-12-20 22:32:26 +0300

    Task 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (!argv[2]) {
+  console.log('No argument');
+} else {
+  argv.slice(2).forEach(element => console.log(element));
+}
