67a4a1c09b989639387dfec4c145f1260d94577b
VaudKK <vaudkeith@gmail.com>
2022-12-20 17:47:37 +0300

    Task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+'use strict';
+
+const { argv } = require('node:process');
+
+if (argv.length > 2) {
+  if (argv.length === 3) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
+} else {
+  console.log('No argument');
+}
