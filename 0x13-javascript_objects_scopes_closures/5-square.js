3eb5a0ffa067f503ca9a3240c23e03b4ad494052
VaudKK <vaudkeith@gmail.com>
2022-12-21 09:57:56 +0300

    Task 5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+'use strict';
+
+const Rectangle = require('./4-rectangle');
+
+export default class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
