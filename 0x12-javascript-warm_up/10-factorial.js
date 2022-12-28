8a2809e1c366200aea708cf6eec2b3f309ac6bb4
VaudKK <vaudkeith@gmail.com>
2022-12-28 17:05:10 +0300

    Task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const input = parseInt(process.argv[2]);
+
+function factorial (number) {
+  if (number === 1 || isNaN(number)) {
+    return 1;
+  }
+
+  return number * factorial(number - 1);
+}
+
+console.log(factorial(input));
