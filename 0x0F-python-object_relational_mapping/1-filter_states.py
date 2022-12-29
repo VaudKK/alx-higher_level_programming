060e1498e13dfb73470d7ba7cd5b7c478dff7257
VaudKK <vaudkeith@gmail.com>
2022-12-29 22:00:40 +0300

    Task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+select all rows from states by start letter
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    arguments = sys.argv[1:]
+    if len(arguments) == 3:
+        db = MySQLdb.connect(host='localhost', user=arguments[0],
+                             passwd=arguments[1], db=arguments[2])
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%'\
+                    order by states.id ASC")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+
+        cur.close()
+        db.close()
