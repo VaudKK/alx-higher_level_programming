afc8baa07755a72404b2e444ffe2dc2f12db8b80
VaudKK <vaudkeith@gmail.com>
2023-01-02 12:42:10 +0300

    Task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+all rows from states by user input prevent sql injection
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    arguments = sys.argv[1:]
+    if len(arguments) == 4:
+        db = MySQLdb.connect(host='localhost', user=arguments[0],
+                             passwd=arguments[1], db=arguments[2])
+        cur = db.cursor()
+        cur.execute("""SELECT * FROM states where states.name LIKE BINARY %(username)s\
+                    order by states.id ASC""",{
+                    'username': arguments[3]
+                 })
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+
+        cur.close()
+        db.close()
