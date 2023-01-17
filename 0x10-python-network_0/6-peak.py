38853c0cc979c50ac78ed90de5ffc9fc174d61ec
VaudKK <vaudkeith@gmail.com>
2023-01-17 22:16:05 +0300

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+"""
+Get peak value with lowerst complexity
+"""
+
+
+def find_peak(list_of_integers):
+    """Find peak"""
+    if len(list_of_integers) == 0:
+        return None
+
+    for i,n in enumerate(list_of_integers):
+        for y,m in enumerate(list_of_integers):
+            if list_of_integers[i] > list_of_integers[y]:
+                temp = m
+                list_of_integers[y] = n
+                list_of_integers[i] = temp
+    
+    return list_of_integers[0]
