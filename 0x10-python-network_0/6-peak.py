6d0e96b81f93229a5d095ff4ba6fcb1792fd9d21
VaudKK <vaudkeith@gmail.com>
2023-01-17 22:20:33 +0300

    Task 6 pep issues

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,11 +9,11 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return None
 
-    for i,n in enumerate(list_of_integers):
-        for y,m in enumerate(list_of_integers):
+    for i, n in enumerate(list_of_integers):
+        for y, m in enumerate(list_of_integers):
             if list_of_integers[i] > list_of_integers[y]:
                 temp = m
                 list_of_integers[y] = n
                 list_of_integers[i] = temp
-    
+
     return list_of_integers[0]
